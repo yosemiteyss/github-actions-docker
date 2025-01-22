e9911b224598a27ee1576386b1625e5080529077
yosemiteyss <kevinhonasdf@gmail.com>
2025-01-22 17:57:39 +0800

    move delete offline runners to start script

--- start.sh
+++ start.sh
@@ -6,7 +6,26 @@ echo "Get registration token for: ${REPO}"
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${TOKEN}" -H "Accept: application/vnd.github+json" \
     "https://api.github.com/repos/${REPO}/actions/runners/registration-token" | jq .token --raw-output)
 
-# Configure runner
+# Get list of offline runners
+echo "Get offline runners for: ${REPO}"
+OFFLINE_RUNNER_LIST=$(curl -H "Authorization: token ${TOKEN}" -H "Accept: application/vnd.github+json" \
+    "https://api.github.com/repos/${REPO}/actions/runners" | jq '[.runners[] | select(.status | contains("offline")) | {id: .id}]')
+
+# Loop through each runner and delete it
+for id in $(echo "$OFFLINE_RUNNER_LIST" | jq -r '.[] | @base64'); do
+    _jq() {
+        echo "${id}" | base64 --decode | jq -r "${1}"
+    }
+
+    RUNNER_ID=$(_jq '.id')
+    echo "Deleting runner with ID: ${RUNNER_ID}"
+
+    # Delete the runner
+    curl -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: token ${TOKEN}" \
+        "https://api.github.com/repos/${REPO}/actions/runners/${RUNNER_ID}"
+done
+
+# Configure new runner
 echo "Start configure runner..."
 cd /home/runner/actions-runner
 ./config.sh --url "https://github.com/${REPO}" --token "${REG_TOKEN}"
